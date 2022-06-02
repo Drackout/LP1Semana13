0c81a281eb558c2fb5298093421861941c2e3bc6
Micael Teixeira <a22103816@alunos.ulht.pt>
2022-06-02 15:59:56 +0200

    ex1 progress

---
+++ Palindrome/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void isPlaindromo(string args)
+        {
+            if (args == null)
+            {
+                throw new ArgumentNullException(args);
+            }
+            
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+                if (arg.Length < 2)
+                {
+                    //return true;
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    isPlaindromo(arg);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {   
+                     
+            isPlaindromo(args);
+            
+
+        }
+    }
+}
